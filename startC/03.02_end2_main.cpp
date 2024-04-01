fe1ecb799c73bbe7a16f995d56e225cd455250d1
Gyeong_Min <104749073+J0Min@users.noreply.github.com>
2024-04-02 01:26:47 +0900

    2024-04-01

--- startC/03.02_end2_main.cpp
+++ startC/03.02_end2_main.cpp
@@ -1,26 +1,26 @@
-#include <iostream>
-#include <cstring>
-using namespace std;
-//string 말고 char[]로 풀이를 하더라~
-class Printer {
-private:
-	string str;
-public:
-	void SetString(string _string);
-	void ShowString();
-};
-void Printer::SetString(string _string) {
-	str = _string;
-}
-void Printer::ShowString(){
-	cout << str << endl;
-}
-int main(void) {
-	Printer pnt;
-	pnt.SetString("Hello world!");
-	pnt.ShowString();
-
-	pnt.SetString("I Love C++");
-	pnt.ShowString();
-	return 0;
-}
+//#include <iostream>
+//#include <cstring>
+//using namespace std;
+////string 말고 char[]로 풀이를 하더라~
+//class Printer {
+//private:
+//	string str;
+//public:
+//	void SetString(string _string);
+//	void ShowString();
+//};
+//void Printer::SetString(string _string) {
+//	str = _string;
+//}
+//void Printer::ShowString() {
+//	cout << str << endl;
+//}
+//int main(void) {
+//	Printer pnt;
+//	pnt.SetString("Hello world!");
+//	pnt.ShowString();
+//
+//	pnt.SetString("I Love C++");
+//	pnt.ShowString();
+//	return 0;
+//}
